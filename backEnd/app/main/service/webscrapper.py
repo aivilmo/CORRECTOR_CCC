5068d92be82fc83a09c9973e58c0033fb42d95f9
Aitana Villaplana <aitanagirlpotter@gmail.com>
2021-06-10 19:49:46 +0200

    merge revert

--- backEnd/app/main/service/webscrapper.py
+++ backEnd/app/main/service/webscrapper.py
@@ -32,6 +32,7 @@ class WebScrapper:
 
     def __init__(self):
 <<<<<<< HEAD
+<<<<<<< HEAD
         self.config_ccc = CCCConfiguration.getInstance().ccc_config
         self.config_path = PathConfiguration.getInstance().routes
         self.logger = Logger.getInstance()
@@ -50,6 +51,9 @@ class WebScrapper:
             executable_path=self.config_path.chrome_driver, chrome_options=self.chrome_options
         )
         self.TIMEOUT = 3
+=======
+        self.logger = Logger()
+>>>>>>> parent of 347c85a (Merge pull request #6 from aivilmo/file_conf)
 =======
         self.logger = Logger()
 >>>>>>> parent of 347c85a (Merge pull request #6 from aivilmo/file_conf)
